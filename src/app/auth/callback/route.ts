b6c2cf18c49be91d8cd4f9d28d689cbdfd1afbf2
haramishra <haraprasad45@gmail.com>
2023-08-31 16:18:15 +0530

    feat(auth): user auth added
    auth ui and supabase auth added

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,20 @@
+import { cookies } from "next/headers"
+import { NextResponse, type NextRequest } from "next/server"
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+
+import type { Database } from "@/types/types_db"
+
+// export const runtime = 'edge'
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get("code")
+
+  if (code) {
+    const supabase = createRouteHandlerClient<Database>({ cookies })
+    await supabase.auth.exchangeCodeForSession(code)
+  }
+
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(requestUrl.origin)
+}
