4cb1ca80c25d6926ddc8f48326cf7cdd5e6b9990
haramishra <haraprasad45@gmail.com>
2023-09-08 11:54:57 +0530

    feat(nav): new link added to nav

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -14,7 +14,7 @@ export async function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav />
+        <MainNav items={session?.user && siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             {!session?.user ? (
