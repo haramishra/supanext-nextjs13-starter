4a1758bd3b1a0c415419f31f1fb24257dbc17c0d
haramishra <haraprasad45@gmail.com>
2023-09-12 00:47:14 +0530

    refactor(typo): userDetails typo fixed

--- src/types/tables_db.ts
+++ src/types/tables_db.ts
@@ -5,4 +5,4 @@ import { Database } from "./types_db"
 export type Subscription = Database["public"]["Tables"]["subscriptions"]["Row"]
 export type Product = Database["public"]["Tables"]["products"]["Row"]
 export type Price = Database["public"]["Tables"]["prices"]["Row"]
-export type userDetails = Database["public"]["Tables"]["users"]["Row"]
+export type UserDetails = Database["public"]["Tables"]["users"]["Row"]
