11b8acc7a14044a2663468ef994b4c52783d4c19
hara <haraprasad45@gmail.com>
2023-09-19 19:45:27 +0530

    fix(loading): pricing loader updated

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,14 @@
 import { SectionContainer } from "@/components/ui/section-container"
 import { CardSkeleton } from "@/components/loaders/card-skeleton"
+import PricingCardSkeleton from "@/components/loaders/priceing-card-skeleton"
 
 export default function DashboardBillingLoading() {
   return (
     <SectionContainer>
-      <CardSkeleton />
+      <div className="flex gap-10">
+        <PricingCardSkeleton />
+        <PricingCardSkeleton />
+      </div>
     </SectionContainer>
   )
 }
